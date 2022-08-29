9510924e68bac6d1d8467616f9b10cce94509f31
jayasurya-vj <jayasurya.vjeee21@gmail.com>
2022-08-30 00:20:23 +0530

    vercel

--- middleware/file.js
+++ middleware/file.js
@@ -16,7 +16,7 @@ destination: (req,file,cb)=>{
 filename: (req,file,cb)=>{
     const name=file.originalname.toLowerCase().split(' ').join('-');
     const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null,name+'-'+Date.now().toISOString().replace(/:/g, '-') +'.'+ext);
+    cb(null,name+'-'+Date.now().toString().replace(/:/g, '-') +'.'+ext);
 
 }
 })
