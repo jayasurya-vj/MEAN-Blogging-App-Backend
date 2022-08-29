fd89eb02239073805535909cc56d870e3cadc9e4
jayasurya-vj <jayasurya.vjeee21@gmail.com>
2022-08-29 21:08:41 +0530

    deploy

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use("/api/posts",postsRouter);
 app.use("/api/user",userRouter);
-app.use("",(req,res,next)=>{
+app.use("*",(req,res,next)=>{
   res.sendFile(path.join(__dirname,"angular","index.html"));
 });
 
