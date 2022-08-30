ed39b89741a14124b9f56a3dfb73a7445320c05d
jayasurya-vj <jayasurya.vjeee21@gmail.com>
2022-08-30 21:18:18 +0530

    vercel

--- middleware/file.js
+++ middleware/file.js
@@ -15,7 +15,7 @@ destination: (req,file,cb)=>{
     const isValid= MIME_TYPE_MAP[file.mimetype];
     let error = new Error("Invalid mime type");
     if(isValid) error=null;
-    cb(error,"images");
+    cb(error,path.join(__dirname + '/images'));
 },
 filename: (req,file,cb)=>{
     const name=file.originalname.toLowerCase().split(' ').join('-');
